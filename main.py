0c068be3f4300c2b71eef7cd96a0b53fd668689f
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-10-28 22:34:49 -0700

    Updated the commands for each button

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ clicked = True
 
 def button_clicked(button):
     global clicked
-    if button['text'] == '' and clicked:
-        text.set("X")
+    if button['text'] == '' and clicked is True:
+        button['text'] = "X"
         clicked = False
-    if button['text'] == ' ' and clicked is False:
-        text.set("O")
+    elif button['text'] == ' ' and clicked is False:
+        button['text'] = "O"
         clicked = True
 
 
@@ -33,31 +33,31 @@ o_player.grid(column=1, row=4)
 second_player = Label(text="Player 2", width=7, height=2)
 second_player.grid(column=0, row=4)
 
-first_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(first_button))
+first_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(first_button))
 first_button.grid(column=0, row=0)
 
-second_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(second_button))
+second_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(second_button))
 second_button.grid(column=1, row=0)
 
-third_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(third_button))
+third_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(third_button))
 third_button.grid(column=2, row=0)
 
-fourth_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(fourth_button))
+fourth_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(fourth_button))
 fourth_button.grid(column=0, row=1)
 
-fifth_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(fifth_button))
+fifth_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(fifth_button))
 fifth_button.grid(column=1, row=1)
 
-sixth_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(sixth_button))
+sixth_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(sixth_button))
 sixth_button.grid(column=2, row=1)
 
-seventh_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(seventh_button))
+seventh_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(seventh_button))
 seventh_button.grid(column=0, row=2)
 
-eight_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda : button_clicked(eight_button))
+eight_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda : button_clicked(eight_button))
 eight_button.grid(column=1, row=2)
 
-ninth_button = Button(textvariable=text, width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(ninth_button))
+ninth_button = Button(text="", width=7, height=5, highlightthickness=2, fg='black', command=lambda: button_clicked(ninth_button))
 ninth_button.grid(column=2, row=2)
 
 window.mainloop()
